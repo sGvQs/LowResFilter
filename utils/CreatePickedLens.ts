f88c97c1335c85afbf52acfe0451de181144dc84
sGvQs <susum.earth@gmail.com>
2025-02-16 15:33:55 +0900

    [main] 一旦レンズ仮決め

--- utils/CreatePickedLens.ts
+++ utils/CreatePickedLens.ts
@@ -11,56 +11,56 @@ export const CreatePickedLens = (
       return {
         name: LensesType.filter_1960s,
         width: imageSize.width * 0.1,
-        compress: 0.1,
+        compress: 0.3 / 12,
         isDefault: false,
       };
     case LensesType.filter_1970s:
       return {
         name: LensesType.filter_1970s,
-        width: imageSize.width * 0.1,
-        compress: 0.1,
+        width: imageSize.width * 0.5,
+        compress: 0.3 / 12,
         isDefault: false,
       };
     case LensesType.filter_1980s:
       return {
         name: LensesType.filter_1980s,
-        width: imageSize.width * 0.1,
-        compress: 0.1,
+        width: imageSize.width,
+        compress: 0.3 / 12,
         isDefault: false,
       };
     case LensesType.filter_1990s:
       return {
         name: LensesType.filter_1990s,
-        width: imageSize.width * 0.1,
-        compress: 0.1,
+        width: imageSize.width,
+        compress: 1 / 12,
         isDefault: false,
       };
     case LensesType.filter_2000s:
       return {
         name: LensesType.filter_2000s,
-        width: imageSize.width * 0.1,
-        compress: 0.1,
+        width: imageSize.width,
+        compress: 8 / 12,
         isDefault: false,
       };
     case LensesType.filter_iphone:
       return {
         name: LensesType.filter_iphone,
-        width: imageSize.width * 0.1,
-        compress: 0.1,
+        width: imageSize.width,
+        compress: 2 / 48,
         isDefault: false,
       };
     case LensesType.filter_iphone_3G:
       return {
         name: LensesType.filter_iphone_3G,
-        width: imageSize.width * 0.1,
-        compress: 0.1,
+        width: imageSize.width,
+        compress: 2 / 12,
         isDefault: false,
       };
     case LensesType.filter_none:
       return {
         name: LensesType.filter_none,
-        width: imageSize.width * 0,
-        compress: 0,
+        width: imageSize.width,
+        compress: 1,
         isDefault: true,
       };
   }
